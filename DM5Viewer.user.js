8947b3f665b3f62466d6f3cab338977aa1163b84
Emma <emma2334@gmail.com>
2018-02-02 01:05:56 +0800

    Update image ajax

--- DM5Viewer.user.js
+++ DM5Viewer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         DM5 Viewer
-// @version      1.0.2
+// @version      1.0.3
 // @description  Display all comic images at once.
 // @author       Emma (emma2334)
 // @match        http://www.dm5.com/m*
@@ -21,9 +21,9 @@
   var count=1;
   var getImg = function(){
     $.ajax({
-      url: "chapterfun.ashx",
-      data: { cid: DM5_CID, page: count, key: $("#dm5_key").val(), language: 1, gtk: 6 },
-      type: "POST",
+      url: 'chapterfun.ashx',
+      data: { cid: DM5_CID, page: count, key:  $("#dm5_key").val(), language: 1, gtk: 6, _cid: DM5_CID, _mid: DM5_MID, _dt: DM5_VIEWSIGN_DT, _sign: DM5_VIEWSIGN },
+      type: 'GET',
       success: function (msg) {
         var img = eval(msg);
         for(i=0; i<img.length; i++){
@@ -35,11 +35,4 @@
     });
   };
   getImg();
-
-  // import css
-  $('head').append('<link rel="stylesheet" href="http://emma2334.github.io/DM5-Viewer/files/css/style.css">');
-
-  // add menu to toolbar
-  $('.rightToolBar').append('<a href="javascript:$(\'.chapterBar.tool\').toggle();" title="目錄" class="logo_6"></a>');
-  $('.chapterBar.down').clone().removeClass('down').addClass('tool').prependTo('.rightToolBar');
 })();
